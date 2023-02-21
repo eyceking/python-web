f31317e800f33fe854434dc43347c74b5fb910a8
songsPlus Lyrics <125839499+eyceking@users.noreply.github.com>
2023-02-21 13:32:25 +0000

    add jobs api

--- app.py
+++ app.py
@@ -1,11 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data scientist',
+    'location': 'Toronto Canada',
+    'salary': 'CAD 120,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Analyst',
+    'location': 'Kingston Jamaica',
+    'salary': 'JDM 190,000'
+  },
+  {
+    'id': 3,
+    'title': 'FrontEnd Develoiper',
+    'location': 'New York USA',
+    'salary': 'USD 110,000'
+  },
+  {
+    'id': 4,
+    'title': 'Backend Developer',
+    'location': 'London Canada'
+  },
+]
 
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, companyName='Zillian')
+  
+@app.route('/api/jobs')
+def jobs_api():
+  return jsonify(JOBS)
 
 
 if __name__ == "__main__":
